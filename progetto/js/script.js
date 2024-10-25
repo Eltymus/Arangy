a0a0bbff3b047cf9d22401a5fda7f19f1fc70e6f
Lorenzo Tripodi <lorenzo.tripodi98@gmail.com>
2024-10-25 18:21:50 +0200

    aggiunta database e costruttore

---
+++ progetto/js/script.js
@@ -0,0 +1,11 @@
+// Constructor for Courses
+class corsi {
+    constructor(name, description , duration, price ,id) {
+      this.name = name;
+      this.duration = duration;
+      this.id = id;
+      this.price = price;
+      this.description = description;
+      this.shortDescription = shortdDescription;
+    }
+  }
